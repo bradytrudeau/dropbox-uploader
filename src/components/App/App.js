64a7d2c7638f3dd2501cf2c0d5f6dc6cfa7b5e38
Brady Trudeau <bradytrudeau@gmail.com>
2021-06-02 16:13:10 -0500

    Uncommented

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,23 +14,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <div id='hero'> */}
           {/* <RecordingAPI/> */}
-          {/* <MobileView> */}
+          <MobileView>
             <div className='mobile'>
               <div id='mobile-inner'>
                 <RecordView/>
               </div>
             </div>
-          {/* </MobileView> */}
-          {/* <BrowserView>
+          </MobileView>
+          <BrowserView>
             <div className='browser'>
               <div id='browser-inner'>
                 <UploadView/>
               </div>
             </div>
-          </BrowserView> */}
-        {/* </div> */}
+          </BrowserView>
       </div>
     );
   }
